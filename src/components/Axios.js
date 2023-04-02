4c03926c270052789511e57617b4fddd1654ad79
Lokesh Rai <lokeshrai2019@iiitkottayam.ac.in>
2023-04-02 19:25:15 +0530

    changes

--- src/components/Axios.js
+++ src/components/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const backEndUrl = "http://172.16.194.51:8080"
-//const backEndUrl = "http://127.0.0.1:8000/"
+//const backEndUrl = "http://172.16.194.51:8080"
+const backEndUrl = "http://127.0.0.1:8000/"
 
 const instance = axios.create({
     baseURL: backEndUrl
